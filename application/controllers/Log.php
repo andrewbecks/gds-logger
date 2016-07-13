6307b07d26d5627360698610bcd47a3df818593d
Nishchal Gautam <gautam.nishchal@gmail.com>
2016-07-13 18:20:49 +0700

    class definition

--- application/controllers/Log.php
+++ application/controllers/Log.php
@@ -1,5 +1,37 @@
 <?php
 
+/**
+ * Class myLog just for type definition stupid PHP doesn't have typing
+ * @var $properties Properties
+ */
+class myLog{
+	/**
+	 * @var $namespace string
+	 */
+	public $namespace;
+	/**
+	 * @var $entity string
+	 */
+	public $entity;
+	/**
+	 * @var $kind string
+	 */
+	public $kind;
+	/**
+	 * @var $properties Properties
+	 */
+	public $properties;
+}
+
+/**
+ * Class Properties just for type definition, I want auto completion on my object too
+ */
+class Properties{
+	public $type;
+	public $done;
+	public $created;
+	public $description;
+}
 class Log {
 	public function index(){
 		if($_SERVER['REQUEST_METHOD'] === 'POST' || $_SERVER['REQUEST_METHOD'] === 'PUT'){
@@ -8,7 +40,15 @@ class Log {
 			$this->get();
 		}
 	}
+
+	/**
+	 * @return myLog
+	 */
+	private function _getLog(){
+		return json_decode(file_get_contents('php://input'));
+	}
 	private function post(){
+		$json = $this->_getLog();
 		$obj = new GDS\Schema('Log');
 		$obj->addString('namespace')
 			->addString('entity')
@@ -17,6 +57,16 @@ class Log {
 			->addBoolean('done')
 			->addDatetime('created')
 			->addString('Description');
+		$log = new GDS\Store($obj);
+		$object = $log->createEntity([
+			'namespace' => '',
+			'entity' => '',
+			'kind' => '',
+			'type' => '',
+			'done' => '',
+			'created' => '',
+			'Description' => ''
+		]);
 	}
 	private function get(){
 
